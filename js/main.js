49251a2d23eca13411ffc5a734f59d0c79326bb0
omarASC5 <ocobas19@avenues.org>
2019-03-03 21:30:34 -0500

    navbar font color changes on scroll

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 let prevScrollpos = window.pageYOffset;
 window.onscroll = () => {
   let currentScrollPos = window.pageYOffset;
-   (prevScrollpos > currentScrollPos) ? 
-	document.getElementById("navbar").classList.remove('shrink') :
-    document.getElementById("navbar").classList.add('shrink');
+   if (prevScrollpos > currentScrollPos) {
+
+	   document.getElementById("navbar").classList.remove('shrink');
+	   document.getElementById("navbar").classList.add('navbar-dark');
+	   document.getElementById("navbar").classList.remove('navbar-light');
+   } else {
+	   document.getElementById("navbar").classList.add('shrink');
+	   document.getElementById("navbar").classList.remove('navbar-dark');
+	   document.getElementById("navbar").classList.add('navbar-light');
+   }
   	prevScrollpos = currentScrollPos;
 }
